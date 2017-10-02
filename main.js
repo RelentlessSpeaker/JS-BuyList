2ca5b539439846374b7626352463e44ddf59c704
RelentlessSpeaker <31990604+RelentlessSpeaker@users.noreply.github.com>
2017-10-02 21:52:00 +0300

    full js

--- main.js
+++ main.js
@@ -5,23 +5,39 @@ $(function () {
     var ONE_ROW_HTML = $(".item-row").html();
 
     var $segment = $(".stats");
-    var ONE_ITEM_HTML = $(".stats-label").html();
+    var ONE_ITEM_HTML = $(".stats").html();
 
-    console.log("One row html", ONE_ITEM_HTML);
+    var $bought = $(".stats-bought");
 
     function addItem(title) {
         var $node = $(ONE_ROW_HTML);
-        var $track = $(ONE_ITEM_HTML);
+        var $node2 = $(ONE_ITEM_HTML);
 
         var quantity = 1;
         var $quantity_label = $node.find(".count-label");
-        var $quantity_label2 = $track.find(".count-label");
+        var $quantity_label2 = $node2.find(".count-label2");
 
         $quantity_label.text(quantity);
+        $quantity_label2.text(quantity);
 
-        $track.find("#product").text(title);
         $node.find(".not-bought").text(title);
+        $node2.find(".title").text(title);
 
+        $node.find(".not-bought").click(function () {
+            $node.find(".not-bought").hide();
+            $node.find(".edit").show();
+            $node.find(".edit").val(title);
+        })
+
+        $node.find(".edit").focusout(function () {
+            $node.find(".edit").hide();
+
+            title = $node.find(".edit").val();
+            $node.find(".edit").val(title);
+            $node.find(".not-bought").text(title);
+            $node.find(".not-bought").show();
+            $node2.find(".title").text(title);
+        })
 
         $node.find(".plus-button").click(function () {
             quantity += 1;
@@ -40,13 +56,15 @@ $(function () {
         $node.find(".delete-button").click(function () {
             $node.fadeOut(250, function () {
                 $node.hide();
-                $track.hide();
+                $node2.hide();
             })
         })
 
         $node.find(".buy-button").click(function () {
             $node.fadeOut(250, function () {
                 $node.addClass("is-bought");
+                $node2.addClass("is-bought");
+                $node2.appendTo($bought);
                 $node.fadeIn(250);
             })
         })
@@ -54,16 +72,18 @@ $(function () {
         $node.find(".unbuy-button").click(function () {
             $node.fadeOut(250, function () {
                 $node.removeClass("is-bought");
+                $node2.removeClass("is-bought");
+                $node2.appendTo($segment);
                 $node.fadeIn(250);
             })
         })
 
-
         $node.css('padding', '17px 10px');
         $node.css('border-bottom', '1px solid rgba(34,36,38,.15)');
+        $node2.css('display', 'inline-block')
 
         $list.append($node);
-        $segment.append($track);
+        $segment.append($node2);
     }
 
     var $new_input = $(".new-item");
@@ -77,4 +97,5 @@ $(function () {
             $new_input.focus();
         }
     })
+
 })
